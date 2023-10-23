54e5c67cae3719932f9643bc0ab6e4abe6317bb5
Esa88 <94520614+Esa88@users.noreply.github.com>
2023-10-23 18:13:23 +0000

    string.js p1

---
+++ p1/con.js
@@ -0,0 +1,8 @@
+let str1="sheikh "
+let str2="esa"
+
+console.log(str1+str2)
+
+console.log(1+"2")
+console.log("1"+2)
+console.log(+true)
---
+++ p1/string.js
@@ -0,0 +1,3 @@
+let name="sheik isah"
+let repo=25
+console.log(`my name is ${name} my repo count is ${repo}`)
